5be8dff9e2864e442e8e8959b5376e59b1f47ae6
rodoggyro <47298960+rodoggyro@users.noreply.github.com>
2022-02-16 17:00:42 -0500

    It works color values need to be changed

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/TeamShippingElementDetector.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/TeamShippingElementDetector.java
@@ -34,7 +34,7 @@ public class TeamShippingElementDetector extends OpenCvPipeline{
     @Override
     public Mat processFrame(Mat input){
         Imgproc.cvtColor(input, mat, Imgproc.COLOR_RGB2HSV);
-        Scalar lowHSV = new Scalar(23,50,70);
+        Scalar lowHSV = new Scalar(23, 50, 70);
         Scalar highHSV = new Scalar(32, 255, 255);
 
         Core.inRange(mat, lowHSV, highHSV, mat);
